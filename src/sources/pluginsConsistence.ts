4df8864c456d60fa73c029181e231a517ec16f97
Rishat Valitov <valitov.rishat@gmail.com>
2021-04-01 17:06:31 +0300

    Plugins signals

--- src/sources/pluginsConsistence.ts
+++ src/sources/pluginsConsistence.ts
@@ -1,15 +1,11 @@
-export default async function arePluginsInconsistent(): Promise<boolean | undefined> {
+export default async function arePluginsConsistent(): Promise<boolean> {
     const { plugins } = navigator
     if (!plugins)
-        return undefined
-
-    let length = plugins.length
-    if (length === 0)
-        return true
+        return false
 
     let isConsistent = Object.getPrototypeOf(plugins) === PluginArray.prototype
     for(let i = 0; i < plugins.length; i++)
         isConsistent &&= Object.getPrototypeOf(plugins[i]) === Plugin.prototype
 
-    return !isConsistent
+    return isConsistent
 }
