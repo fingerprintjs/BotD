9dcb92d66d617a0a7394921603fc39dec2734554
xnerhu <xnerhu@gmail.com>
2022-09-08 13:58:29 +0200

    fix plugins array detector

--- src/detection/plugins-array.ts
+++ src/detection/plugins-array.ts
@@ -2,5 +2,5 @@ import { SignalKind } from '../signals'
 import { BotKind, ComponentDict, DetectionResponse, State } from '../types'
 
 export function detectPluginsArray({ [SignalKind.PluginsArray]: pluginsArray }: ComponentDict): DetectionResponse {
-  if (pluginsArray.state === State.Success && pluginsArray.value) return BotKind.HeadlessChrome
+  if (pluginsArray.state === State.Success && !pluginsArray.value) return BotKind.HeadlessChrome
 }
