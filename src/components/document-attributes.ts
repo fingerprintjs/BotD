57b45f7d03d843d350c2d68dcb96a9a2a7a88199
xnerhu <xnerhu@gmail.com>
2022-09-13 13:06:19 +0200

    Fix ci error

--- src/components/document-attributes.ts
+++ src/components/document-attributes.ts
@@ -4,9 +4,6 @@ export default function getDocumentAttributes(): string[] {
   if (document.documentElement === undefined) {
     throw new BotdError(State.Undefined, 'document.documentElement is undefined')
   }
-  const names: string[] = []
-  for (const attr of document.documentElement.attributes) {
-    names.push(attr.name)
-  }
+  const names = Array.from(document.documentElement.attributes).map((r) => r.name)
   return names
 }
