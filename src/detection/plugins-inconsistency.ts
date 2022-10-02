dc72114fd7ad27ad7d69c15cedd4b2dd66f222e4
xnerhu <xnerhu@gmail.com>
2022-10-02 21:35:01 +0200

    Fix rtt detector on android

--- src/detection/plugins-inconsistency.ts
+++ src/detection/plugins-inconsistency.ts
@@ -3,5 +3,6 @@ import { BotKind, ComponentDict, DetectionResponse, State } from '../types'
 export function detectPluginsLengthInconsistency({ isIPad, pluginsLength }: ComponentDict): DetectionResponse {
   if (isIPad.state !== State.Success || pluginsLength.state !== State.Success) return
   // We need to additionally check for mobile Safari, as it has 0 plugins
-  if (pluginsLength.value === 0 && !isIPad.value) return BotKind.HeadlessChrome
+  if (isIPad.value) return
+  if (pluginsLength.value === 0) return BotKind.HeadlessChrome
 }
--- src/detection/rtt.ts
+++ src/detection/rtt.ts
@@ -1,5 +1,8 @@
 import { BotKind, ComponentDict, DetectionResponse, State } from '../types'
 
-export function detectRTT({ rtt }: ComponentDict): DetectionResponse {
-  if (rtt.state === State.Success && rtt.value === 0) return BotKind.HeadlessChrome
+export function detectRTT({ rtt, isAndroid }: ComponentDict): DetectionResponse {
+  if (rtt.state !== State.Success || isAndroid.state !== State.Success) return
+  // Rtt is 0 on android webview
+  if (isAndroid.value) return
+  if (rtt.value === 0) return BotKind.HeadlessChrome
 }
