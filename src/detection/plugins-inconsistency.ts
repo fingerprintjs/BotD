cf84e9ea24ca528f9a5ce06933f8cfd463243d05
xnerhu <xnerhu@gmail.com>
2022-10-02 23:00:32 +0200

    Fix plugins incosistency on opera mobile

--- src/detection/plugins-inconsistency.ts
+++ src/detection/plugins-inconsistency.ts
@@ -1,8 +1,18 @@
-import { BotKind, ComponentDict, DetectionResponse, State } from '../types'
+import { BotKind, BrowserKind, ComponentDict, DetectionResponse, State } from '../types'
 
-export function detectPluginsLengthInconsistency({ isDesktopSafari, pluginsLength }: ComponentDict): DetectionResponse {
-  if (pluginsLength.state !== State.Success) return
-  // We need to additionally check for mobile Safari, as it has 0 plugins
-  if (isDesktopSafari.state === State.Success && !isDesktopSafari.value) return
+export function detectPluginsLengthInconsistency({
+  browserKind,
+  isAndroid,
+  isDesktopSafari,
+  pluginsLength,
+}: ComponentDict): DetectionResponse {
+  if (pluginsLength.state !== State.Success || browserKind.state !== State.Success || isAndroid.state !== State.Success)
+    return
+  // We need to additionally check for mobile Safari, as it has 0 plugins. Same with mobile opera.
+  if (
+    (isDesktopSafari.state === State.Success && !isDesktopSafari.value) ||
+    (browserKind.value === BrowserKind.Opera && isAndroid.value)
+  )
+    return
   if (pluginsLength.value === 0) return BotKind.HeadlessChrome
 }
