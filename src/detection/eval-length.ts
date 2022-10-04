f9805416e38b3c222f5e02118dd9824e6db48a04
xnerhu <xnerhu@gmail.com>
2022-10-04 19:02:03 +0200

    Omit eval length detection if browser kind is unknown

--- src/detection/eval-length.ts
+++ src/detection/eval-length.ts
@@ -1,13 +1,17 @@
 import { BrowserKind, ComponentDict, DetectionResponse, State } from '../types'
 
 export function detectEvalLengthInconsistency({ evalLength, browserKind }: ComponentDict): DetectionResponse {
-  if (evalLength.state === State.Success && browserKind.state === State.Success) {
-    const length = evalLength.value
-    const browser = browserKind.value
-    return (
-      (length === 37 && ![BrowserKind.Firefox, BrowserKind.Safari].includes(browser)) ||
-      (length === 39 && ![BrowserKind.IE].includes(browser)) ||
-      (length === 33 && ![BrowserKind.Chrome, BrowserKind.Opera].includes(browser))
-    )
-  }
+  if (
+    evalLength.state !== State.Success ||
+    browserKind.state !== State.Success ||
+    browserKind.value === BrowserKind.Unknown
+  )
+    return
+  const length = evalLength.value
+  const browser = browserKind.value
+  return (
+    (length === 37 && ![BrowserKind.Firefox, BrowserKind.Safari].includes(browser)) ||
+    (length === 39 && ![BrowserKind.IE].includes(browser)) ||
+    (length === 33 && ![BrowserKind.Chrome, BrowserKind.Opera].includes(browser))
+  )
 }
