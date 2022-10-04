e31fc6d30d24c5da91224c2e2ee654b644118c15
xnerhu <xnerhu@gmail.com>
2022-10-04 19:35:04 +0200

    Refactor playground

--- playground/index.ts
+++ playground/index.ts
@@ -1,6 +1,16 @@
 import { load } from '../src'
 import './style.css'
 
+interface Result {
+  timeToGetResults: string
+  timeToCollectSignals: string
+  detectionResult: Record<string, any>
+  detailedResults: Record<string, any>
+  collectedData: Record<string, any>
+}
+
+let result: Result | undefined = undefined
+
 const runDetection = async () => {
   const statusEl = document.getElementById('status')
   const resultTableEl = document.getElementById('result-table')
@@ -24,29 +34,38 @@ const runDetection = async () => {
     const instance = await load()
     const collectTime = performance.now() - t0
 
-    const result = instance.detect()
+    const detectionResult = instance.detect()
     const processTime = performance.now() - t0
 
+    const components = instance.get() ?? {}
+    const detectors = instance.getDetectorsResponses() ?? {}
+
+    result = {
+      timeToGetResults: collectTime.toFixed(0) + ' ms',
+      timeToCollectSignals: processTime.toFixed(0) + ' ms',
+      detectionResult,
+      collectedData: components,
+      detailedResults: detectors,
+    }
+
+    sourcesEl!.textContent = JSON.stringify(result.collectedData, null, 4)
     errorSectionEl!.style.display = 'none'
     resultsSectionEl!.style.display = 'block'
 
-    perfEl!.textContent = collectTime.toFixed(0) + ' ms'
-    timeEl!.textContent = processTime.toFixed(0) + ' ms'
-
-    const components = instance.get()
-    sourcesEl!.textContent = JSON.stringify(components, null, 4)
+    perfEl!.textContent = result.timeToGetResults
+    timeEl!.textContent = result.timeToCollectSignals
 
     statusEl!.textContent = 'Result:'
-    resultEl!.textContent = JSON.stringify(result, null, 4)
+    resultEl!.textContent = JSON.stringify(result.detectionResult, null, 4)
 
     detailedResultsEl!.textContent = 'Detailed results:'
-    detailedResultsEl!.textContent = JSON.stringify(instance.getDetectorsResponses(), null, 4)
+    detailedResultsEl!.textContent = JSON.stringify(result.detailedResults, null, 4)
 
     const resultTableEntries = [
       [
         { text: 'automationTool' },
-        result.bot ? { text: '<b>Detected</b>', class: 'green' } : { text: '<b>Not detected</b>' },
-        result.bot ? { text: `<b>${result.botKind}</b>`, class: 'green' } : null,
+        detectionResult.bot ? { text: '<b>Detected</b>', class: 'green' } : { text: '<b>Not detected</b>' },
+        detectionResult.bot ? { text: `<b>${detectionResult.botKind}</b>`, class: 'green' } : null,
       ],
     ]
 
@@ -72,6 +91,12 @@ const runDetection = async () => {
 
 window.onload = async () => {
   document.getElementById('detect-button')!.addEventListener('click', () => runDetection())
+
+  document.getElementById('copy-logs-button')!.addEventListener('click', () => {
+    navigator.clipboard.writeText(JSON.stringify(result)).then(() => {
+      alert('Copied to clipboard')
+    })
+  })
 
   runDetection()
 }
