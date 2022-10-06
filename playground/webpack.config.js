e31973f0cb716c244e40d380a2ed8286da12823a
xnerhu <xnerhu@gmail.com>
2022-10-06 21:48:43 +0200

    Rename static back to resources

--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -36,7 +36,7 @@ const config = {
       template: `playground/index.html`,
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: 'playground/static' }],
+      patterns: [{ from: 'resources' }],
     }),
   ],
 
