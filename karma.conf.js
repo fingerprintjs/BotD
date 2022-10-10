17d2e944d098c2ac7a4ec1b207a2583c93fdae6d
xnerhu <xnerhu@gmail.com>
2022-10-10 19:48:19 +0200

    reduce browserstack timeout

--- karma.conf.js
+++ karma.conf.js
@@ -123,7 +123,7 @@ function setupBrowserstack(config) {
       // https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables#default-environment-variables
       build: process.env.GITHUB_RUN_ID || makeBuildNumber(),
       // The timeout is reduced for testing sessions to not hold the BrowserStack queue long in case of problems.
-      timeout: 5 * 60,
+      timeout: 120,
     },
   })
 }
