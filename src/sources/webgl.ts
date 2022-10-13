df2f17e35152fe31d7957849da51ea431c70ab17
xnerhu <xnerhu@gmail.com>
2022-10-13 17:13:36 +0200

    simplify webgl source

--- src/sources/webgl.ts
+++ src/sources/webgl.ts
@@ -2,19 +2,13 @@ import { BotdError, State } from '../types'
 import { getMozAppearanceSupport } from '../utils/browser'
 
 export interface WebGLPayload {
-  version: string
   vendor: string
   renderer: string
-  shadingLanguageVersion: string
-  vendorUnmasked: string
-  rendererUnmasked: string
 }
 
 export interface WebGLMozPayload {
-  version: string
   vendor: string
   renderer: string
-  shadingLanguageVersion: string
 }
 
 export default function getWebGL(): WebGLPayload | WebGLMozPayload {
@@ -34,18 +28,12 @@ export default function getWebGL(): WebGLPayload | WebGLMozPayload {
     throw new BotdError(State.NotFunction, 'WebGLRenderingContext.getParameter is not a function')
   }
 
-  const version = webGLContext.getParameter(webGLContext.VERSION)
   const vendor = webGLContext.getParameter(webGLContext.VENDOR)
   const renderer = webGLContext.getParameter(webGLContext.RENDERER)
-  const shadingLanguageVersion = webGLContext.getParameter(webGLContext.SHADING_LANGUAGE_VERSION)
 
-  if (getMozAppearanceSupport())
-    return {
-      version: version,
-      vendor: vendor,
-      renderer: renderer,
-      shadingLanguageVersion: shadingLanguageVersion,
-    }
+  if (getMozAppearanceSupport()) {
+    return { vendor: vendor, renderer: renderer }
+  }
 
   const webGLDebugInfo = webGLContext.getExtension('WEBGL_debug_renderer_info')
 
@@ -53,15 +41,5 @@ export default function getWebGL(): WebGLPayload | WebGLMozPayload {
     throw new BotdError(State.Null, 'WEBGL_debug_renderer_info extension is null')
   }
 
-  const unmaskedVendor = webGLContext.getParameter(webGLDebugInfo.UNMASKED_VENDOR_WEBGL)
-  const unmaskedRenderer = webGLContext.getParameter(webGLDebugInfo.UNMASKED_RENDERER_WEBGL)
-
-  return {
-    version: version,
-    vendor: vendor,
-    renderer: renderer,
-    shadingLanguageVersion: shadingLanguageVersion,
-    vendorUnmasked: unmaskedVendor,
-    rendererUnmasked: unmaskedRenderer,
-  }
+  return { vendor: vendor, renderer: renderer }
 }
