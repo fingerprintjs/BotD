db92b38230d8b81ca3027d7972424c2e8b955198
xnerhu <xnerhu@gmail.com>
2022-10-13 18:53:20 +0200

    ignore android browsers and mobile safari in plugins length detector

--- src/detectors/plugins_inconsistency.ts
+++ src/detectors/plugins_inconsistency.ts
@@ -1,14 +1,14 @@
-import { BotKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
-import { getBrowserKind, isAndroid, isDesktopSafari } from '../utils/browser'
+import { BotKind, BrowserEngineKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
+import { getBrowserEngineKind, getBrowserKind, isAndroid, isDesktopSafari } from '../utils/browser'
 
 export function detectPluginsLengthInconsistency({ pluginsLength }: ComponentDict): DetectorResponse {
   if (pluginsLength.state !== State.Success) return
-  // We need to additionally check for mobile Safari, as it has 0 plugins. Same with mobile opera.
   const browserKind = getBrowserKind()
+  const browserEngineKind = getBrowserEngineKind()
+  // Chromium based android browsers and mobile safari have 0 plugins length.
   if (
-    isDesktopSafari() ||
-    (browserKind === BrowserKind.Opera && isAndroid()) ||
-    (browserKind === BrowserKind.WeChat && isAndroid())
+    (browserEngineKind === BrowserEngineKind.Chromium && isAndroid()) ||
+    (browserKind === BrowserKind.Safari && !isDesktopSafari())
   )
     return
   if (pluginsLength.value === 0) return BotKind.HeadlessChrome
