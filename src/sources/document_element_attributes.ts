ef9e8282639d25c9901c23131f40c0b3120037c6
Rishat Valitov <rishat.valitov@fingerprint.com>
2022-12-20 18:10:23 +0800

    Updates for pro agent (#108)
    * Update README.md
    
    * Update some signals and add type exports
    
    * Remove duplicate signals and fix lint

--- src/sources/document_element_attributes.ts
+++
@@ -1,8 +0,0 @@
-import { BotdError, State } from '../types'
-
-export default function getDocumentAttributes(): string[] {
-  if (document.documentElement === undefined) {
-    throw new BotdError(State.Undefined, 'document.documentElement is undefined')
-  }
-  return Array.from(document.documentElement.attributes).map((r) => r.name)
-}
