588daa27d418b65a9703d45a80db0090d071a7dd
fpkamp <117085311+fpkamp@users.noreply.github.com>
2023-02-22 15:44:11 +0100

    migrate configs to .ts

---
+++ karma.conf.ts
@@ -0,0 +1,195 @@
+import { Config, CustomLauncher } from 'karma'
+import { KarmaTypescriptConfig } from 'karma-typescript/dist/api/configuration'
+import { karmaPlugin, setHttpsAndServerForKarma } from '@fpjs-incubator/broyster/node'
+
+declare module 'karma' {
+  interface ConfigOptions {
+    karmaTypescriptConfig?: KarmaTypescriptConfig | undefined
+  }
+
+  interface Config {
+    preset?: string
+    reporters: ConfigOptions['reporters']
+  }
+}
+
+// The shapes of these objects are taken from:
+// https://github.com/SeleniumHQ/selenium/tree/d8ddb4d83972df0f565ef65264bcb733e7a94584/javascript/node/selenium-webdriver
+// It doesn't work, trying to work it out with BrowserStack support. Todo: solve it with the support.
+/*
+const chromeIncognitoCapabilities = {
+  'goog:chromeOptions': {
+    args: ['--incognito'],
+  },
+}
+const firefoxIncognitoCapabilities = {
+  'moz:firefoxOptions': {
+    prefs: {
+      'browser.privatebrowsing.autostart': true,
+    },
+  },
+}
+*/
+
+/*
+ * You can find values for any supported browsers in the interactive form at
+ * https://www.browserstack.com/docs/automate/javascript-testing/configure-test-run-options
+ * The keys are arbitrary values.
+ *
+ * Only Chrome is supported on Android, only Safari is supported on iOS: https://www.browserstack.com/question/659
+ */
+/* eslint-disable max-len */
+// prettier-ignore
+/*
+const browserstackBrowsers = {
+  IE11: { platform: 'Windows', osVersion: '7', browserName: 'IE', browserVersion: '11.0', useHttps: true },
+  Windows11_EdgeLatest: { platform: 'Windows', osVersion: '11', browserName: 'Edge', browserVersion: 'latest-beta', useHttps: true },
+  Windows10_Chrome49: { platform: 'Windows', osVersion: '10', browserName: 'Chrome', browserVersion: '49.0', useHttps: true },
+  // Windows10_Chrome49_Incognito: { platform: 'Windows', osVersion: '10', browserName: 'Chrome', browserVersion: '49.0', ...chromeIncognitoCapabilities },
+  Windows11_ChromeLatest: { platform: 'Windows', osVersion: '11', browserName: 'Chrome', browserVersion: 'latest-beta', useHttps: true },
+  // Windows11_ChromeLatest_Incognito: { platform: 'Windows', osVersion: '11', browserName: 'Chrome', browserVersion: 'latest-beta, ...chromeIncognitoCapabilities },
+  Windows10_Firefox52: { platform: 'Windows', osVersion: '10', browserName: 'Firefox', browserVersion: '52.0', useHttps: true },
+  // Windows10_Firefox52_Incognito: { platform: 'Windows', osVersion: '10', browserName: 'Firefox', browserVersion: '52.0', ...firefoxIncognitoCapabilities },
+  Windows11_FirefoxLatest: { platform: 'Windows', osVersion: '11', browserName: 'Firefox', browserVersion: 'latest-beta', useHttps: true },
+  // Windows11_FirefoxLatest_Incognito: { platform: 'Windows', osVersion: '11', browserName: 'Firefox', browserVersion: 'latest-beta, ...firefoxIncognitoCapabilities },
+  OSXMojave_Safari12: { platform: 'OS X', osVersion: 'Mojave', browserName: 'Safari', browserVersion: '12.1', useHttps: true },
+  OSXMonterey_Safari15: { platform: 'OS X', osVersion: 'Monterey', browserName: 'Safari', browserVersion: '15.0', useHttps: false },
+  OSXMonterey_ChromeLatest: { platform: 'OS X', osVersion: 'Monterey', browserName: 'Chrome', browserVersion: 'latest-beta', useHttps: true },
+  // OSXMonterey_ChromeLatest_Incognito: { platform: 'OS X', osVersion: 'Monterey', browserName: 'Chrome', browserVersion: 'latest-beta, ...chromeIncognitoCapabilities },
+  OSXMonterey_FirefoxLatest: { platform: 'OS X', osVersion: 'Monterey', browserName: 'Firefox', browserVersion: 'latest-beta', useHttps: true },
+  // OSXMonterey_FirefoxLatest_Incognito: { platform: 'OS X', osVersion: 'Monterey', browserName: 'Firefox', browserVersion: 'latest-beta, ...firefoxIncognitoCapabilities },
+  OSXMonterey_EdgeLatest: { platform: 'OS X', osVersion: 'Monterey', browserName: 'Edge', browserVersion: 'latest-beta', useHttps: true },
+  Android11_ChromeLatest: { deviceName: 'Google Pixel 4', os: 'Android', osVersion: '11.0', browserName: 'Chrome', browserVersion: 'latest-beta', useHttps: true },
+  iOS10_Safari: { deviceName: 'iPhone 7', os: 'iOS', osVersion: '10', browserName: 'Safari' , useHttps: true},
+  iOS11_Safari: { deviceName: 'iPhone 8 Plus', os: 'iOS', osVersion: '11', browserName: 'Safari', useHttps: true },
+  iOS12_Safari: { deviceName: 'iPhone XS', os: 'iOS', osVersion: '12', browserName: 'Safari', useHttps: true },
+  iOS13_Safari: { deviceName: 'iPhone 11 Pro', os: 'iOS', osVersion: '13', browserName: 'Safari', useHttps: true },
+  iOS14_Safari: { deviceName: 'iPhone 11', os: 'iOS', osVersion: '14', browserName: 'Safari', useHttps: true },
+  // iOS15_Safari: { deviceName: 'iPhone 11 Pro', os: 'iOS', osVersion: '15', browserName: 'Safari' }
+}
+*/
+
+const browserstackBrowsers = {
+  Windows10_Chrome57: { platform: 'Windows', osVersion: '10', browserName: 'Chrome', browserVersion: '57', useHttps: true },
+  // Windows10_Chrome57_Incognito: { platform: 'Windows', osVersion: '10', browserName: 'Chrome', browserVersion: '57', ...chromeIncognitoCapabilities },
+  Windows11_ChromeLatest: { platform: 'Windows', osVersion: '11', browserName: 'Chrome', browserVersion: 'latest-beta', useHttps: true },
+  // Windows11_ChromeLatest_Incognito: { platform: 'Windows', osVersion: '11', browserName: 'Chrome', browserVersion: 'latest-beta, ...chromeIncognitoCapabilities },
+  Windows10_Firefox67: { platform: 'Windows', osVersion: '10', browserName: 'Firefox', browserVersion: '67', useHttps: true },
+  // Windows10_Firefox67_Incognito: { platform: 'Windows', osVersion: '10', browserName: 'Firefox', browserVersion: '67', ...firefoxIncognitoCapabilities },
+  Windows11_FirefoxLatest: { platform: 'Windows', osVersion: '11', browserName: 'Firefox', browserVersion: 'latest-beta', useHttps: true },
+  // Windows11_FirefoxLatest_Incognito: { platform: 'Windows', osVersion: '11', browserName: 'Firefox', browserVersion: 'latest-beta, ...firefoxIncognitoCapabilities },
+  Windows11_EdgeLatest: { platform: 'Windows', osVersion: '11', browserName: 'Edge', browserVersion: 'latest-beta', useHttps: true },
+  'OSX10.14_Safari12': { platform: 'OS X', osVersion: 'Mojave', browserName: 'Safari', browserVersion: '12', useHttps: true },
+  OSX12_Safari15: { platform: 'OS X', osVersion: 'Monterey', browserName: 'Safari', browserVersion: '15', useHttps: false },
+  OSX13_Safari16: { platform: 'OS X', osVersion: 'Ventura', browserName: 'Safari', browserVersion: '16', useHttps: false },
+  OSX13_ChromeLatest: { platform: 'OS X', osVersion: 'Ventura', browserName: 'Chrome', browserVersion: 'latest-beta', useHttps: true },
+  // OSX13_ChromeLatest_Incognito: { platform: 'OS X', osVersion: 'Ventura', browserName: 'Chrome', browserVersion: 'latest-beta, ...chromeIncognitoCapabilities },
+  OSX13_FirefoxLatest: { platform: 'OS X', osVersion: 'Ventura', browserName: 'Firefox', browserVersion: 'latest-beta', useHttps: true },
+  // OSX13_FirefoxLatest_Incognito: { platform: 'OS X', osVersion: 'Ventura', browserName: 'Firefox', browserVersion: 'latest-beta, ...firefoxIncognitoCapabilities },
+  OSX13_EdgeLatest: { platform: 'OS X', osVersion: 'Ventura', browserName: 'Edge', browserVersion: 'latest-beta', useHttps: true },
+  Android13_ChromeLatest: { deviceName: ['Google Pixel 7', 'Google Pixel 7 Pro', 'Google Pixel 6 Pro'], platform: 'Android', osVersion: '13.0', browserName: 'Chrome', browserVersion: 'latest-beta', useHttps: true },
+  iOS11_Safari: { deviceName: ['iPhone 8 Plus', 'iPhone 6S', 'iPhone 8', 'iPhone 6'], platform: 'iOS', osVersion: '11', browserName: 'Safari', useHttps: true },
+  iOS12_Safari: { deviceName: ['iPhone XS', 'iPhone 6S', 'iPhone 8 Plus', 'iPhone XR'], platform: 'iOS', osVersion: '12', browserName: 'Safari', useHttps: true },
+  iOS13_Safari: { deviceName: ['iPhone 11 Pro', 'iPhone 8', 'iPhone XS', 'iPhone 11 Pro Max'], platform: 'iOS', osVersion: '13', browserName: 'Safari', useHttps: true },
+  iOS14_Safari: { deviceName: ['iPhone 11', 'iPhone XS', 'iPhone 12 Pro', 'iPhone 12 mini'], platform: 'iOS', osVersion: '14', browserName: 'Safari', useHttps: true },
+  iOS15_Safari: { deviceName: ['iPhone 13', 'iPhone 13 Mini', 'iPhone 11 Pro', 'iPhone 11'], platform: 'iOS', osVersion: '15', browserName: 'Safari', useHttps: true },
+  iOS16_Safari: { deviceName: ['iPhone 14', 'iPhone 14 Pro Max', 'iPhone 14 Pro', 'iPhone 14 Plus'], platform: 'iOS', osVersion: '16', browserName: 'Safari', useHttps: true},
+}
+
+/* eslint-enable max-len */
+
+function makeBuildNumber() {
+  return `No CI ${Math.floor(Math.random() * 1e10)}`
+}
+
+function setupLocal(config: Config) {
+  const files = ['src/**/*.ts', 'tests/**/*.ts', 'dist/fp.min.js']
+
+  config.set({
+    frameworks: ['jasmine', 'karma-typescript'],
+    files: [
+      // The polyfills are required for old supported browsers.
+      // They should be removed when the old browser support is dropped.
+      'node_modules/promise-polyfill/dist/polyfill.js',
+
+      'src/**/*.ts',
+      'tests/**/*.ts',
+      'test-dist/botd.min.js',
+    ],
+    preprocessors: {
+      '**/*.ts': 'karma-typescript',
+    },
+    reporters: ['spec', 'summary'],
+    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
+    concurrency: 3,
+
+    karmaTypescriptConfig: {
+      tsconfig: 'tsconfig.browser.json',
+      compilerOptions: {
+        module: 'commonjs',
+        sourceMap: true,
+      },
+      include: files,
+    },
+
+    specReporter: {
+      suppressSummary: true,
+      suppressErrorSummary: true,
+      suppressPassed: true,
+      suppressSkipped: true,
+    },
+
+    summaryReporter: {
+      show: 'skipped', // To know that some tests are skipped exactly where they are supposed to be skipped
+    },
+  })
+}
+
+function setupBrowserstack(config: Config) {
+  setupLocal(config)
+
+  const customLaunchers: Record<string, CustomLauncher> = {}
+  for (const [key, data] of Object.entries(browserstackBrowsers)) {
+    customLaunchers[key] = {
+      base: 'BrowserStack',
+      name: key.replace(/_/g, ' '),
+      ...data,
+    }
+  }
+
+  config.set({
+    reporters: [...(config.reporters || []), 'BrowserStack'],
+    plugins: [karmaPlugin, 'karma-*'],
+    browsers: Object.keys(customLaunchers),
+    customLaunchers,
+    concurrency: 5,
+    failOnEmptyTestSuite: false,
+    retryLimit: 3,
+    captureTimeout: 15_000,
+    browserStack: {
+      project: 'BotD',
+      // A build number is required to group testing sessions in the BrowserStack UI.
+      // GitHub Actions will add a value for GITHUB_RUN_ID. More on the environment variables:
+      // https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables#default-environment-variables
+      build: process.env.GITHUB_RUN_ID || makeBuildNumber(),
+      // The timeout is reduced for testing sessions to not hold the BrowserStack queue long in case of problems.
+      idleTimeout: 20_000,
+      queueTimeout: 300_000,
+    },
+  })
+  setHttpsAndServerForKarma(config)
+}
+
+/**
+ * Add `--preset local` or `--preset browserstack` to the Karma command to choose where to run the tests.
+ */
+module.exports = (config: Config) => {
+  switch (config.preset) {
+    case 'local':
+      return setupLocal(config)
+    case 'browserstack':
+      return setupBrowserstack(config)
+    default:
+      throw new Error('No --preset option is set or an unknown value is set')
+  }
+}
---
+++ rollup.config.ts
@@ -0,0 +1,103 @@
+import * as fs from 'fs'
+import type { RollupOptions } from 'rollup'
+import jsonPlugin from '@rollup/plugin-json'
+import nodeResolvePlugin from '@rollup/plugin-node-resolve'
+import typescriptPlugin from '@rollup/plugin-typescript'
+import terserPlugin from '@rollup/plugin-terser'
+import dtsPlugin from 'rollup-plugin-dts'
+import licensePlugin from 'rollup-plugin-license'
+import terserConfig from './terser.config'
+
+const { dependencies } = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+
+const outputDirectory = 'dist'
+const tsconfig = 'tsconfig.browser.json'
+
+const commonInput = {
+  input: 'src/index.ts',
+  plugins: [nodeResolvePlugin(), jsonPlugin(), typescriptPlugin({ tsconfig })],
+}
+
+const commonOutput = {
+  name: 'FingerprintJS',
+  exports: 'named' as const,
+  plugins: [
+    licensePlugin({
+      banner: {
+        content: {
+          file: 'resources/license_banner.txt',
+        },
+      },
+    }),
+  ],
+}
+
+const commonTerser = terserPlugin(terserConfig)
+
+const config: RollupOptions[] = [
+  // Browser bundles. They have all the dependencies included for convenience.
+  {
+    ...commonInput,
+    output: [
+      // IIFE for users who use Require.js or Electron and want to just call `window.FingerprintJS.load()`
+      {
+        ...commonOutput,
+        file: `${outputDirectory}/botd.js`,
+        format: 'iife',
+      },
+      {
+        ...commonOutput,
+        file: `${outputDirectory}/botd.min.js`,
+        format: 'iife',
+        plugins: [commonTerser, ...commonOutput.plugins],
+      },
+
+      // UMD for users who use Require.js or Electron and want to leverage them
+      {
+        ...commonOutput,
+        file: `${outputDirectory}/botd.umd.js`,
+        format: 'umd',
+      },
+      {
+        ...commonOutput,
+        file: `${outputDirectory}/botd.umd.min.js`,
+        format: 'umd',
+        plugins: [commonTerser, ...commonOutput.plugins],
+      },
+    ],
+  },
+
+  // NPM bundles. They have all the dependencies excluded for end code size optimization.
+  {
+    ...commonInput,
+    external: Object.keys(dependencies),
+    output: [
+      // CJS for usage with `require()`
+      {
+        ...commonOutput,
+        file: `${outputDirectory}/botd.cjs.js`,
+        format: 'cjs',
+      },
+
+      // ESM for usage with `import`
+      {
+        ...commonOutput,
+        file: `${outputDirectory}/botd.esm.js`,
+        format: 'esm',
+      },
+    ],
+  },
+
+  // TypeScript definition
+  {
+    ...commonInput,
+    plugins: [dtsPlugin({ tsconfig })],
+    output: {
+      ...commonOutput,
+      file: `${outputDirectory}/botd.d.ts`,
+      format: 'esm',
+    },
+  },
+]
+
+export default config
