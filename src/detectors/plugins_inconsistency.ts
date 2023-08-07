d3076c132e798026d50e38e69b89c17e800e636a
Mikołaj Palkiewicz <xnerhu@gmail.com>
2023-08-07 21:14:05 +0200

    ignore wechat in plugins incosistency (#137)

--- src/detectors/plugins_inconsistency.ts
+++ src/detectors/plugins_inconsistency.ts
@@ -1,13 +1,15 @@
-import { BotKind, BrowserEngineKind, ComponentDict, DetectorResponse, State } from '../types'
-import { getBrowserEngineKind, isAndroid, isDesktopSafari } from '../utils/browser'
+import { BotKind, BrowserEngineKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
+import { getBrowserEngineKind, getBrowserKind, isAndroid, isDesktopSafari } from '../utils/browser'
 
 export function detectPluginsLengthInconsistency({ pluginsLength }: ComponentDict): DetectorResponse {
   if (pluginsLength.state !== State.Success) return
   const browserEngineKind = getBrowserEngineKind()
+  const browserKind = getBrowserKind()
   // Chromium based android browsers and mobile webkit based browsers have 0 plugins length.
   if (
     (browserEngineKind === BrowserEngineKind.Chromium && isAndroid()) ||
-    (browserEngineKind === BrowserEngineKind.Webkit && !isDesktopSafari())
+    (browserEngineKind === BrowserEngineKind.Webkit && !isDesktopSafari()) ||
+    browserKind === BrowserKind.WeChat
   )
     return
   if (pluginsLength.value === 0) return BotKind.HeadlessChrome
