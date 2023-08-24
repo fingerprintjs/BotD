1a0cc6aae04f432c94eabb0c93dbaf4830f6bcf9
Mikołaj Palkiewicz <xnerhu@gmail.com>
2023-08-24 13:05:00 +0200

    whitelist chrome in plugins length incosistency (#140)

--- src/detectors/plugins_inconsistency.ts
+++ src/detectors/plugins_inconsistency.ts
@@ -1,16 +1,9 @@
-import { BotKind, BrowserEngineKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
-import { getBrowserEngineKind, getBrowserKind, isAndroid, isDesktopSafari } from '../utils/browser'
+import { BotKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
+import { getBrowserKind, isAndroid } from '../utils/browser'
 
 export function detectPluginsLengthInconsistency({ pluginsLength }: ComponentDict): DetectorResponse {
   if (pluginsLength.state !== State.Success) return
-  const browserEngineKind = getBrowserEngineKind()
   const browserKind = getBrowserKind()
-  // Chromium based android browsers and mobile webkit based browsers have 0 plugins length.
-  if (
-    (browserEngineKind === BrowserEngineKind.Chromium && isAndroid()) ||
-    (browserEngineKind === BrowserEngineKind.Webkit && !isDesktopSafari()) ||
-    browserKind === BrowserKind.WeChat
-  )
-    return
+  if (browserKind !== BrowserKind.Chrome || isAndroid()) return
   if (pluginsLength.value === 0) return BotKind.HeadlessChrome
 }
