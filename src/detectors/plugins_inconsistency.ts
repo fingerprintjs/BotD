28727f29ec21ce79dff98bfa5b544decc3b239be
Mikołaj Palkiewicz <xnerhu@gmail.com>
2023-09-09 15:21:04 +0200

    fix detectPluginsLengthInconsistency (#142)

--- src/detectors/plugins_inconsistency.ts
+++ src/detectors/plugins_inconsistency.ts
@@ -1,9 +1,10 @@
-import { BotKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
-import { getBrowserKind, isAndroid } from '../utils/browser'
+import { BotKind, BrowserEngineKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
+import { getBrowserEngineKind, getBrowserKind, isAndroid } from '../utils/browser'
 
 export function detectPluginsLengthInconsistency({ pluginsLength }: ComponentDict): DetectorResponse {
   if (pluginsLength.state !== State.Success) return
   const browserKind = getBrowserKind()
-  if (browserKind !== BrowserKind.Chrome || isAndroid()) return
+  const browserEngineKind = getBrowserEngineKind()
+  if (browserKind !== BrowserKind.Chrome || isAndroid() || browserEngineKind !== BrowserEngineKind.Chromium) return
   if (pluginsLength.value === 0) return BotKind.HeadlessChrome
 }
