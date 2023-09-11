2056ef48dbfc4877cb4d2bbed40b2f9d2a1271a3
Leo Developer <leodeveloper@protonmail.com>
2023-09-11 13:36:20 +0200

    fix merge

--- src/detectors/plugins_inconsistency.ts
+++ src/detectors/plugins_inconsistency.ts
@@ -1,4 +1,4 @@
-import { BotKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
+import { BotKind, BrowserEngineKind, BrowserKind, ComponentDict, DetectorResponse, State } from '../types'
 
 export function detectPluginsLengthInconsistency({
   pluginsLength,
@@ -6,6 +6,6 @@ export function detectPluginsLengthInconsistency({
 }: ComponentDict): DetectorResponse {
   if (pluginsLength.state !== State.Success || browser.state !== State.Success) return
 	const { isAndroid, browserKind, browserEngineKind } = browser.value
-  if (browserKind !== BrowserKind.Chrome || isAndroid() || browserEngineKind !== BrowserEngineKind.Chromium) return
+  if (browserKind !== BrowserKind.Chrome || isAndroid || browserEngineKind !== BrowserEngineKind.Chromium) return
   if (pluginsLength.value === 0) return BotKind.HeadlessChrome
 }
