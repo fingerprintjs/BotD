428a70d174f28e0c6ae807d9494928d4891412f5
Sergey M. <finesserus@gmail.com>
2024-10-18 18:02:19 +1000

    Fix the tests

--- src/sources/document_element_keys.test.ts
+++ src/sources/document_element_keys.test.ts
@@ -1,17 +1,8 @@
-import { getBrowserMajorVersion, isChromium } from '../../tests/utils'
-import { BotdError } from '../types'
 import getDocumentElementKeys from './document_element_keys'
 
 describe('Sources', () => {
   describe('documentElementKeys', () => {
     it('returns expected values or throws', () => {
-      if (isChromium() && (getBrowserMajorVersion() ?? 0) < 61) {
-        expect(() => getDocumentElementKeys()).toThrow(
-          new BotdError(-2, 'document.documentElement.getAttributeNames is not a function'),
-        )
-        return
-      }
-
       const result = getDocumentElementKeys()
       // There are no document keys when running the tests using Karma
       expect(result).toEqual([])
--- src/sources/webdriver.test.ts
+++ src/sources/webdriver.test.ts
@@ -1,15 +1,9 @@
-import { getBrowserMajorVersion, isChromium, isHeadlessChrome } from '../../tests/utils'
+import { isHeadlessChrome } from '../../tests/utils'
 import getWebDriver from './webdriver'
-import { BotdError } from '../types'
 
 describe('Sources', () => {
   describe('webdriver', () => {
     it('returns an expected value or throws', () => {
-      if (isChromium() && (getBrowserMajorVersion() ?? 0) < 63) {
-        expect(() => getWebDriver()).toThrow(new BotdError(-1, 'navigator.webdriver is undefined'))
-        return
-      }
-
       const result = getWebDriver()
 
       if (isHeadlessChrome()) {
